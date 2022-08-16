ac8e831894b7dd2c0f41b220bf15c7bcb6b2028d
fakontello <fakontello@gmail.com>
2022-08-16 18:12:51 +0300

    refactor 3

--- src/test/java/praktikum/BurgerTests.java
+++ src/test/java/praktikum/BurgerTests.java
@@ -2,40 +2,12 @@ package praktikum;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
-@RunWith(MockitoJUnitRunner.class)
 public class BurgerTests {
-
-    @Mock
-    Burger burger;
-
-    @Test
-    public void SetBunsTest() {
-        // Arrange
-        Bun bun = new Bun("с кунжутом", 12);
-        // Act
-        burger.setBuns(bun);
-        // Assert
-        Mockito.verify(burger).setBuns(bun);
-    }
-
-    @Test
-    public void AddIngredientTest() {
-        // Arrange
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "кепчук", 56);
-        // Act
-        burger.addIngredient(ingredient);
-        // Assert
-        Mockito.verify(burger).addIngredient(ingredient);
-    }
 
     @Test
     public void RemoveIngredientTest() {
